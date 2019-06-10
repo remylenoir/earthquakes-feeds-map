cc3b27a44a09252a1a756e0c86e79674a91495b4
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-06-10 11:51:56 +0200

    Utilities folder refactored

---
+++ src/utilitiess/utilities/Colors/magnitude.js
@@ -0,0 +1,12 @@
+export const magColors = {
+  mag0: '#ebebeb',
+  mag1: '#ffffaf',
+  mag2: '#fffe4b',
+  mag3: '#feff01',
+  mag4: '#ffe202',
+  mag5: '#ffd101',
+  mag6: '#ffad00',
+  mag7: '#f96400',
+  mag8: '#fe0000',
+  mag9: '#800000'
+};
