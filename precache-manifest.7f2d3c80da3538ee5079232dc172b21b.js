7d5e511a1beca98c42ff0de348616822fd258a10
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-06-15 10:06:15 +0200

    Updates

---
+++ precache-manifest.7f2d3c80da3538ee5079232dc172b21b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698912912a0a17e11be4168fc9f0858",
+    "url": "/earthquakes-feeds-map/index.html"
+  },
+  {
+    "revision": "5d8e8a59f070316a4a7f",
+    "url": "/earthquakes-feeds-map/static/css/2.2614ebd1.chunk.css"
+  },
+  {
+    "revision": "a04d1b12d2ef8ce9bbc7",
+    "url": "/earthquakes-feeds-map/static/css/main.d8311ff5.chunk.css"
+  },
+  {
+    "revision": "5d8e8a59f070316a4a7f",
+    "url": "/earthquakes-feeds-map/static/js/2.4867bfae.chunk.js"
+  },
+  {
+    "revision": "a04d1b12d2ef8ce9bbc7",
+    "url": "/earthquakes-feeds-map/static/js/main.f28d279b.chunk.js"
+  },
+  {
+    "revision": "a5f9bee21ad814d77e78",
+    "url": "/earthquakes-feeds-map/static/js/runtime~main.9d6202bc.js"
+  }
+]);
